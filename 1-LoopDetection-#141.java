2c9859f35123853861bb3d13e7a4ae45a6ea8e2f
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-02-10 17:58:59 +0530

    Create 1-LoopDetection-#141.java

---
+++ 1-LoopDetection-#141.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow_pointer = head, fast_pointer = head;
+        while (fast_pointer != null && fast_pointer.next != null) {
+            slow_pointer = slow_pointer.next;
+            fast_pointer = fast_pointer.next.next;
+            if (slow_pointer == fast_pointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
