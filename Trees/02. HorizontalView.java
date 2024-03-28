4dbf178ecc648bff5bad9800d8b26aa05763a1dc
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-03-28 14:48:27 +0530

    Update 02. HorizontalView.java

--- Trees/02. HorizontalView.java
+++ Trees/02. HorizontalView.java
@@ -1,66 +1,54 @@
 import java.util.*;
+
 class TreeNode {
-char val;
-TreeNode left;
-TreeNode right;
-public TreeNode(char val) {
-this.val = val;
-left = null;
-right = null;
-}}
-public class Main {
-// Function to obtain the Horizontal
-View of a binary tree
-public static List<Character>
-horizontalView(TreeNode root) {
-List<Character> horizontalView =
-new ArrayList<>();
-if (root == null) {
-return horizontalView;
+    char val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(char val) {
+        this.val = val;
+    }
 }
-Queue<TreeNode> queue = new
-LinkedList<>();
-queue.offer(root);
-while (!queue.isEmpty()) {
-int levelSize = queue.size();
-for (int i = 0; i <
-levelSize; i++) {
-TreeNode node =
-queue.poll();
-horizontalView.add(node.val);
-if (node.left != null) {
-queue.offer(node.left);
-}
-if (node.right != null) {
-queue.offer(node.right);
-}
-}
-}return horizontalView;
-}
-public static void main(String[]
-args) {
-// Sample binary tree input
-TreeNode root = new
-TreeNode('A');
-root.left = new TreeNode('B');
-root.right = new TreeNode('C');
-root.left.left = new
-TreeNode('D');
-root.left.right = new
-TreeNode('E');
-root.right.left = new
-TreeNode('F');
-root.right.right = new
-TreeNode('G');
-List<Character> horizontalViewResult =
-horizontalView(root);
-// Printing the Horizontal View
-System.out.print("Horizontal
-View: ");
-for (char node :
-horizontalViewResult) {
-System.out.print(node + " ");
-}
-System.out.println();
-}
+
+public class HorizontalView {
+    public static List<Character> horizontalView(TreeNode root) {
+        List<Character> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            result.add(node.val);
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Sample binary tree input
+        TreeNode root = new TreeNode('A');
+        root.left = new TreeNode('B');
+        root.right = new TreeNode('C');
+        root.left.left = new TreeNode('D');
+        root.right.left = new TreeNode('E');
+        root.right.right = new TreeNode('F');
+
+        // Obtain the horizontal view of the binary tree
+        List<Character> horizontalViewResult = horizontalView(root);
+
+        // Printing the Horizontal View
+        System.out.println("Horizontal View:");
+        for (char node : horizontalViewResult) {
+            System.out.print(node + " ");
+        }
+    }
 }
