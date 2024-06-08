cf45f7f6fe318954838db29b5c9a3ba2aa4bc041
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-06-08 19:49:31 +0530

    Update 01. FibonacciSeq.java

--- DynamicProgramming/01. FibonacciSeq.java
+++ DynamicProgramming/01. FibonacciSeq.java
@@ -1,12 +1,16 @@
 public class Fibonacci {
 
 public static int fibonacciBottomUp(int n) {
+//Base xase
 if (n <= 1) {
 return n;
 }
+//store the older values
 int[] fibArray = new int[n + 1];
+//initialize the array created
 fibArray[0] = 0;
 fibArray[1] = 1;
+fill the full array also
 for (int i = 2; i <= n; i++) {
 fibArray[i] = fibArray[i-1] + fibArray[i-2];
 }
